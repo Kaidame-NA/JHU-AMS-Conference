97075d188fcad570b1868288078158fa205a116a
Edward Wang <ewang43@jhu.edu>
2022-07-24 19:56:03 -0400

    Finish hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'heritage-blue': '#002D72',
+				'spirit-blue': '#68ACE5',
+			},
+		},
 	},
 	plugins: [],
 	darkMode: 'class',
