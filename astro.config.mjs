71fd3984da3fe6d10600e66468ef8b4f37743a92
Edward Wang <ewang43@jhu.edu>
2022-07-24 21:13:49 -0400

    Set up to deploy on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
-  site: "https://tailwind-astro.vercel.app",
+  site: "https://kaidame-na.github.io",
+  base: '/JHU-AMS-Conference',
   integrations: [
     tailwind({
       config: {
